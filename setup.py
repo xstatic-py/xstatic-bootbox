d9a76229b591b79a4a0c0ae6aa4c775e744f053a
Thomas Waldmann <tw@waldmann-edv.de>
2018-09-18 21:07:51 +0200

    initial import (as of xstatic-bootbox 4.3.0.1)

---
+++ setup.py
@@ -0,0 +1,28 @@
+from xstatic.pkg import bootbox as xs
+
+# The README.txt file should be written in reST so that PyPI can use
+# it to generate your project's PyPI page. 
+with open('README.txt') as f:
+    long_description = f.read()
+
+from setuptools import setup, find_packages
+
+setup(
+    name=xs.PACKAGE_NAME,
+    version=xs.PACKAGE_VERSION,
+    description=xs.DESCRIPTION,
+    long_description=long_description,
+    classifiers=xs.CLASSIFIERS,
+    keywords=xs.KEYWORDS,
+    maintainer=xs.MAINTAINER,
+    maintainer_email=xs.MAINTAINER_EMAIL,
+    license=xs.LICENSE,
+    url=xs.HOMEPAGE,
+    platforms=xs.PLATFORMS,
+    packages=find_packages(),
+    namespace_packages=['xstatic', 'xstatic.pkg', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],  # nothing! :)
+                          # if you like, you MAY use the 'XStatic' package.
+)
